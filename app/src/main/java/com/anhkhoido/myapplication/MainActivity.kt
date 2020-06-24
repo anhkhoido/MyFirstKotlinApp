0c96ebb3857c36877a0dcadfa6cb1f3bb8ed9589
anhkhoido <anhkhoido@gmail.com>
2020-06-24 13:01:06 -0400

    activities - Managed my first transition for to another activity

--- app/src/main/java/com/anhkhoido/myapplication/MainActivity.kt
+++ app/src/main/java/com/anhkhoido/myapplication/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.anhkhoido.myapplication
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    fun enterLobby(view: View) {
+        buttonMainActivity.setOnClickListener {
+            val intent = Intent(this, LobbyActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
